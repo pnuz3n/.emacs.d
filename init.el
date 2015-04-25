5ce6ba86b25140f8783fe16bbd7fb7b4314585fc
Panu Wetterstrand <panu.wetterstrand@iki.fi>
2015-04-25 16:18:36 +0000

    Removed bars

--- init.el
+++ init.el
@@ -7,9 +7,9 @@
 (setq inhibit-splash-screen t)
 
 ;; Turn off mouse interface early in startup to avoid momentary display
-;(if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
-;(if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
-;(if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
+(if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
+(if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
+(if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
 
 (setq settings-dir
       (expand-file-name "settings" user-emacs-directory))
