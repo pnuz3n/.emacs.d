e1d8415581f31194a343896e69e51cc6bf2c2a30
Panu Wetterstrand <panu.wetterstrand@iki.fi>
2015-04-25 15:46:35 +0000

    Go setup is done only if go is installed

--- settings/setup-go.el
+++ settings/setup-go.el
@@ -1,17 +1,27 @@
+(defun system-has-go () "Tests wether go is installed or not" 
+       (condition-case nil
+           (progn
+             (start-process "" nil "go")
+             t
+             )
+         (error nil))
+       )
+(defun setup-go () "Install go environment with el-get"
+       (el-get-bundle go-mode)
+       (el-get-bundle let-alist)
+       (el-get-bundle dash)
+       (el-get-bundle flycheck)
+       ;; go get github.com/dougm/goflymake
+       (add-to-list 'load-path "~/src/github.com/dougm/goflymake")
+       (require 'go-flycheck)
 
-(el-get-bundle go-mode)
 
-(el-get-bundle let-alist)
-(el-get-bundle dash)
-(el-get-bundle flycheck)
-;; go get github.com/dougm/goflymake
-(add-to-list 'load-path "~/src/github.com/dougm/goflymake")
-(require 'go-flycheck)
+       ;; go get github.com/nsf/gocode
+       (el-get-bundle go-autocomplete)
+       (require 'go-autocomplete)
 
 
-;; go get github.com/nsf/gocode
-(el-get-bundle go-autocomplete)
-(require 'go-autocomplete)
+       (add-hook 'before-save-hook 'gofmt-before-save)
+       )
 
-
-(add-hook 'before-save-hook 'gofmt-before-save)
+(if (system-has-go) (setup-go))
