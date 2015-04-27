408a2cf68732d9503d95a3c7d7f11b33f11ebf12
Panu Wetterstrand <panu.wetterstrand@iki.fi>
2015-04-27 08:07:45 +0300

    Jumping back for Ace mode

--- settings/setup-acejump.el
+++ settings/setup-acejump.el
@@ -7,4 +7,14 @@
                 ))
 
                  
-
+;; 
+;; enable a more powerful jump back function from ace jump mode
+;;
+(autoload
+  'ace-jump-mode-pop-mark
+  "ace-jump-mode"
+  "Ace jump back:-)"
+  t)
+(eval-after-load "ace-jump-mode"
+  '(ace-jump-mode-enable-mark-sync))
+(define-key global-map (kbd "C-x SPC") 'ace-jump-mode-pop-mark)
