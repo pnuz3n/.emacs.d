a38a80d7c43662eaf54c70cf669ba7a887a4cfce
Panu Wetterstrand <panu.wetterstrand@iki.fi>
2015-06-22 22:11:20 +0300

    El-get build melpa repositories

--- lisp/setup-el-get.el
+++ lisp/setup-el-get.el
@@ -7,6 +7,11 @@
     (goto-char (point-max))
     (eval-print-last-sexp)))
 
+(require 'el-get-elpa)
+;; Build the El-Get copy of the package.el packages if we have not
+;; built it before.  Will have to look into updating later ...
+(unless (file-directory-p el-get-recipe-path-elpa)
+  (el-get-elpa-build-local-recipes))
 
 (add-to-list 'el-get-recipe-path "~/.emacs.d/el-get-user/recipes")
 (el-get 'sync)
