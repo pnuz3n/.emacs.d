e376a8892f1439b07c47a729860a9ce05a03c283
Panu Wetterstrand <panu.wetterstrand@iki.fi>
2015-10-06 06:46:01 +0000

    Added script to open file in Emacs and moving point to given line and column

---
+++ sh/openinemacs.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# From https://developer.atlassian.com/blog/2015/03/emacs-intellij/
+# Opens file in Emacs in given position.
+#
+file=$1
+line=$2
+col=$3
+/usr/bin/emacsclient -e "(progn 
+       (find-file \"$file\")
+
+       ;; Jump to the same point as in IntelliJ
+       ;; Unfortunately, IntelliJ doesn't always supply the values
+       ;; depending on where the open is invoked from; e.g. keyboard 
+       ;; works, tab context doesn't
+       (when (not (string= \"\" \"$line\"))
+         (goto-char (point-min))
+         (forward-line (1- $2))
+         (forward-char (1- $3)))
+
+       ;; Raise/focus our window; depends on the windowing system
+       (if (string-equal system-type \"darwin\")
+         (ns-do-applescript \"tell application \\\"Emacs\\\" to activate\")
+         (raise-frame))
+​
+       ;; Automatically pick up changes made in IntelliJ
+       (auto-revert-mode t))"
+
+     )"
