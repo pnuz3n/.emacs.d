a56bc91dcd65d360d5f759c85a309e4cef57a81a
Panu Wetterstrand <panu.wetterstrand@iki.fi>
2019-08-30 11:33:45 +0300

    SSH agent usage works with TRAMP ssh again
    The way ssh agent environment was set up has changed from earlier
    versions. There is no longer configuration file where agent socket could be
    read.

--- init.el
+++ init.el
@@ -26,25 +26,8 @@
 
 (add-to-list 'load-path "~/.emacs.d/lisp/")
 
-;; read gpg-agent environment
-
-(defun read-env-line (line)
-  "read a env line and post to environment"
-  (let ((key-value-pair (split-string line "=" t)))
-    (setenv (car key-value-pair) (car (last key-value-pair))))
-  )
-(defvar gpg-agent-info-file)
-(setq gpg-agent-info-file (concat (getenv "HOME") "/.gpg-agent-info"))
-
-(when (and
-       (eq system-type 'darwin)
-       (file-exists-p gpg-agent-info-file))
-      (with-temp-buffer
-        (progn
-          (insert-file-contents gpg-agent-info-file)
-          (mapc 'read-env-line (split-string (buffer-string) "\n" t)))
-        )
-  )
+(when (eq system-type 'darwin)
+(setenv "SSH_AUTH_SOCK" (concat (getenv "HOME") "/.gnupg/S.gpg-agent.ssh")))
 
 (add-to-list 'load-path "~/.emacs.d/el-get/el-get")
 
